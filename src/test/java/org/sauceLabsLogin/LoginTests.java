7a75ab3d35dc05f232a61848135757aaf53d946e
Rohit Prasad <pdrohit674@gmail.com>
2024-03-24 16:56:36 +0530

    initial commit

---
+++ src/test/java/org/sauceLabsLogin/LoginTests.java
@@ -0,0 +1,21 @@
+package org.sauceLabsLogin;
+
+import java.lang.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class LoginTests {
+    WebDriver wd;
+    @BeforeSuite
+    public void setup(){
+        wd = new ChromeDriver();
+        wd.get("https://www.saucedemo.com/");
+    }
+    @Test
+    public void checkUrl(){
+        String url = wd.getTitle();
+        Assert.assertEquals(url, "Swag Labs", "Incorrect Title");
+    }
+}
