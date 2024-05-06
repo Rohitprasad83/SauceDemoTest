088b10fb3695ac6cd5d4c4b1f94794359a7034ee
Rohit Prasad <pdrohit674@gmail.com>
2024-05-06 22:21:57 +0530

    added login method in LoginPage

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -39,4 +39,10 @@ public class LoginPage extends BasePage{
     public void clickSubmit(){
         submitBtn.click();
     }
+
+    public void login(String username, String password){
+        this.setUserName(username);
+        this.setPassword(password);
+        this.clickSubmit();
+    }
 }
